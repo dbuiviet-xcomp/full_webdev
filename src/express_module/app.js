796495dc37976fac50b54559e8cf530e9b62e841
Dung Bui Viet <dbuiviet.xcomp@gmail.com>
2024-07-20 15:13:09 +0000

    Added EJS template

--- src/express_module/app.js
+++ src/express_module/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+//var pug = require('pug');
+var ejs = require('ejs');
 
 var port = 3000;
 var app = express();
@@ -10,16 +12,26 @@ app.use(function(req, res, next){
     next();
 });
 
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send('Hello Express!');
+    //res.send('Hello Express!');
+    res.render('index', {
+        title: 'Hello EJS',
+        body: 'This is body of the view',
+        show: true,
+        people: ['John', 'Donald', 'Steve']
+    });
 });
 
 app.get('/about', function(req, res){
-    res.send('About Page!');
+    res.render('about');
 });
 
 app.listen(port);
